337c7ef110a9c681b17cbad428e1e16f30b799cb
Dante Hawler <202559257+D-Hawler@users.noreply.github.com>
2025-04-02 03:35:08 +0300

    core mechanics
    the basic mechanics are ready

--- script.js
+++ script.js
@@ -26,139 +26,155 @@ start.onclick = () => {
     gameChoice.appendChild(textChoice);
 
     const choice = document.createElement("div");
-    choice.classList.add("choice")
+    choice.classList.add("choice");
 
     gameChoice.appendChild(choice);
 
     const rock = document.createElement("button");
-    rock.id = "rock";
     rock.textContent = "Rock";
 
     const paper = document.createElement("button");
-    paper.id = "paper";
     paper.textContent = "Paper";
 
     const scissors = document.createElement("button");
-    scissors.id = "scissors";
     scissors.textContent = "Scissors";
 
     choice.appendChild(rock);
     choice.appendChild(paper);
     choice.appendChild(scissors);
 
+    const scoreGame = document.createElement("section");
+    scoreGame.classList.add("scoreGame");
+
+    container.appendChild(scoreGame);
+
+    const textScore = document.createElement("h1");
+    textScore.textContent = "Game score:";
+
+    const score = document.createElement("div");
+    score.classList.add("choice");
+
+    scoreGame.appendChild(textScore);
+    scoreGame.appendChild(score);
+
+    const scoreP = document.createElement("div");
+    scoreP.textContent = scorePlayrs;
+    scoreP.id = "players";
+    scoreP.classList.add("score");
+
+    const scoreC = document.createElement("div");
+    scoreC.textContent = scoreComputer;
+    scoreC.id = "computer";
+    scoreC.classList.add("score");
+
+    score.appendChild(scoreP);
+    score.appendChild(scoreC);
+
     start.onclick = null;
+
+    rock.onclick = () => playRound("rock");
+    paper.onclick = () => playRound("paper");
+    scissors.onclick = () => playRound("scissors");
 };
 
+function playRound(human) {
+    const computer = getComputerChoice();
+    cardChanges(human, computer);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function getComputerChoice() {
-//     let x = parseInt(Math.random() * 3);1
-
-//     return numberToMove(x);
-// }
-
-// function getHumanChoice() {
-//     let x = parseInt(window.prompt("Keep a count: 0 - Stone, 1 - paper, 2 - Scissors"));
-//     return numberToMove(x);
-// }
-// function numberToMove(x) {
-//     switch (x) {
-//         case 0:
-//             return "Rock";
-//             break;
-//         case 1:
-//             return "Paper";
-//             break;
-//         case 2:
-//             return "Scissors";
-//             break;
-//     }
-// }
-
-// function playRound(human, computer) {
-//     if (human === undefined) {
-//         console.log(`Wrong number`);
-//         console.log(`--------------------------------------------------------`);
-//     }
-
-//     if (human === computer) {
-//         console.log(`Draw both chose: ${human}`)
-//         console.log(`--------------------------------------------------------`);
-//     }
+    if (human === computer) {
+        console.log(`Draw both chose: ${human}`)
+    }
     
-//     if (human === "Rock" && computer === "Scissors") {
-//         return won();
-//     }else if (human === "Paper" && computer === "Rock") {
-//         return won();
-//     }else if (human === "Scissors" && computer === "Paper") {
-//         return won();
-//     }
+    if (human === "rock" && computer === "scissors") {
+        return won();
+    }else if (human === "paper" && computer === "rock") {
+        return won();
+    }else if (human === "scissors" && computer === "paper") {
+        return won();
+    }
 
-//     if (computer === "Rock" && human === "Scissors") {
-//         return lose();
-//     }else if (computer === "Paper" && human === "Rock") {
-//         return lose();
-//     }else if (computer === "Scissors" && human === "Paper") {
-//         return lose();
-//     }
-// }
-// function won() {
-//     if (humanScore < 4) {
-//         console.log(`You won this round`);
-//         humanScore++;
-//         console.log(`--------------------------------------------------------`);
-//         console.log(`Score: you - ${humanScore}, opponent - ${computerScore}.`);
-//         console.log(`--------------------------------------------------------`);
-//     } else {
-//         console.log(`You won, congratulations`);
-//         humanScore++;
-//         console.log(`--------------------------------------------------------`);
-//         console.log(`Final score: you - ${humanScore}, opponent - ${computerScore}.`);
-//         console.log(`--------------------------------------------------------`);
-//     }
-// }
-// function lose() {
-//     if (computerScore < 4) {
-//         console.log(`You lost this round`);
-//         computerScore++;
-//         console.log(`--------------------------------------------------------`);
-//         console.log(`Score: you - ${humanScore}, opponent - ${computerScore}.`);
-//         console.log(`--------------------------------------------------------`);
-//     } else {
-//         console.log(`You lost, maybe you'll get lucky next time`);
-//         computerScore++;
-//         console.log(`--------------------------------------------------------`);
-//         console.log(`Final score: you - ${humanScore}, opponent - ${computerScore}.`);
-//         console.log(`--------------------------------------------------------`);
-//     }
-// }
+    if (computer === "rock" && human === "scissors") {
+        return lose();
+    }else if (computer === "paper" && human === "rock") {
+        return lose();
+    }else if (computer === "scissors" && human === "paper") {
+        return lose();
+    }
+}
 
-// let humanScore = 0;
-// let computerScore = 0;
+let scorePlayrs = 0;
+let scoreComputer = 0;
 
-// function playGame() {
-//     while (humanScore < 5 && computerScore < 5) {
-//         let human = getHumanChoice();
-//         let computer = getComputerChoice();
+function won() {
+    scorePlayrs++;
+    const scoreP = document.querySelector("#players");
+    scoreP.textContent = scorePlayrs;
+}
 
-//         console.log(`Your choice: ${human}`)
-//         console.log(`Opponent's choice: ${computer}`)
+function lose() {
+    scoreComputer++;
+    const scoreC = document.querySelector("#computer");
+    scoreC.textContent = scoreComputer;
+}
 
-//         playRound(human, computer);
-//     }
-// }
+function score() {
 
-// playGame();
+}
+
+function getComputerChoice() {
+    let x = parseInt(Math.random() * 3);1
+
+    switch (x) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+function cardChanges(human, computer) {
+    const cardPlayrs = document.querySelector(".cardPlayrs");
+    const cardComputer = document.querySelector(".cardComputer");
+
+    while (cardPlayrs.firstChild) {
+        cardPlayrs.removeChild(cardPlayrs.firstChild);
+    }
+    while (cardComputer.firstChild) {
+        cardComputer.removeChild(cardComputer.firstChild);
+    }
+
+    const cardImgPlayers = document.createElement("img");
+    switch (human) {
+        case "rock":
+            cardImgPlayers.setAttribute("src", "./img/Rock.jpg");
+            cardPlayrs.appendChild(cardImgPlayers); 
+            break;
+        case "paper":
+            cardImgPlayers.setAttribute("src", "./img/Paper.jpg");
+            cardPlayrs.appendChild(cardImgPlayers); 
+            break;
+        case "scissors":
+            cardImgPlayers.setAttribute("src", "./img/Scissors.jpg");
+            cardPlayrs.appendChild(cardImgPlayers); 
+            break;
+    }
+
+    const cardImgComputer = document.createElement("img");
+    cardImgComputer.style.transform = "scaleX(-1)";
+    switch (computer) {
+        case "rock":
+            cardImgComputer.setAttribute("src", "./img/Rock.jpg");
+            cardComputer.appendChild(cardImgComputer); 
+            break;
+        case "paper":
+            cardImgComputer.setAttribute("src", "./img/Paper.jpg");
+            cardComputer.appendChild(cardImgComputer); 
+            break;
+        case "scissors":
+            cardImgComputer.setAttribute("src", "./img/Scissors.jpg");
+            cardComputer.appendChild(cardImgComputer); 
+            break;
+    }
+}
